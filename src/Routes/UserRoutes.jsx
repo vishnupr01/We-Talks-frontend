e5956fdcf9f27c31a0778f981287d2cfcd75896d
Vishnu P R <vishnuvicki3$@gmail.com>
2024-09-02 14:38:14 +0530

    hosting new update frontend new9
    Signed-off-by: Vishnu P R <vishnuvicki3$@gmail.com>

--- src/Routes/UserRoutes.jsx
+++ src/Routes/UserRoutes.jsx
@@ -38,17 +38,14 @@ function UserRoutes() {
   return (
 
     <Routes>
-      <Route path="/*" element={<LoginPrivate />}>
-        <Route path="/" element={<LoginPrivate />} />
-        <Route path="500" element={<ServerErrorPage />} />
-        <Route path="changePassword" element={<ForgotPageVerify />} />
-        <Route path="register" element={<RegisterPrivate />} />
-        <Route path="forgotPassword" element={<ForgotEmailVerify />} />
-        <Route path="googleForm" element={<GooglePrivate />} />
-        <Route path="login" element={<LoginPrivate />} />
-        <Route path="otp" element={<OtpPageVerify />} />
-      </Route>
-
+      <Route path='/' element={<LoginPrivate />} />
+      <Route path='500' element={<ServerErrorPage />} />
+      <Route path='changePassword' element={<ForgotPageVerify />} />
+      <Route path="register" element={<RegisterPrivate />} />
+      <Route path='forgotPassword' element={<ForgotEmailVerify />} />
+      <Route path='googleForm' element={<GooglePrivate />} />
+      <Route path="login" element={<LoginPrivate />} />
+      <Route path="otp" element={<OtpPageVerify />} />
 
       <Route element={<PrivateRoute />}>
 
